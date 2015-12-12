c73e060f3a3fc2638ba7c3cb1c7137f6375a1cad
jzajic <jan.zajic@corpus.cz>
2015-12-12 10:16:07 +0100

    first version of tsa server and readme doc

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# tsa-server
+# portable Time Stamp Server (over HTTP)
+
+This is Time-Stamp Protocol via HTTP server (https://tools.ietf.org/html/rfc3161 3.4.)
+https://en.wikipedia.org/wiki/Trusted_timestamping
+
+All operations are based on OpenSSL extension called ts. From version 0.9.8 it is a part of openssl binary so patching is not required anymore.
+It is based on idea from Grzegorz Golec, but his server received request from tcp connection and java libraries used only HTTP to send TSA request.
+
+## USAGE:
+install groovy
+groovy ./server.groovy
+curl -X POST -H "Content-Type:application/timestamp-query" -d @mycertificatefile localhost:318 -v
+
+or call it from Java using class TSAClientBouncyCastle(itext).
+
+## Requirements
+– groovy (and java of cource)
+– OpenSSL > 0.9.8
+– CA or request for certificate
+– certificate for TSA signing
+
+## Installation:
+
+You need ssl key and certificate that can be used for Timestamping. 
+
+This certificate must have attributes:
+keyUsage = nonRepudiation
+extendedKeyUsage = timeStamping, critical
+
+Contant your certification authority to get this cert or for testing create self signed (see below)
+
+Than configure openssl in openssl.cnf:
+```
+[ tsa ]
+
+default_tsa = tsa_config1 # the default TSA section
+
+[ tsa_config1 ]
+
+# These are used by the TSA reply generation only. 
+dir = /etc/ssl/tsa # TSA root directory 
+serial = $dir/serial # The current serial number (mandatory) 
+crypto_device = builtin # OpenSSL engine to use for signing 
+
+signer_cert = $dir/tsa.crt # The TSA signing certificate
+signer_key = $dir/tsa.key # The TSA private key
+
+certs = $dir/cacert.pem # Certificate chain to include in reply # (optional) 
+
+default_policy = tsa_policy1 # Policy if request did not specify it # (optional) 
+other_policies = tsa_policy2, tsa_policy3 # acceptable policies (optional) 
+digests = md5, sha1 # Acceptable message digests (mandatory) 
+accuracy = secs:1, millisecs:500, microsecs:100 # (optional) 
+clock_precision_digits = 0 # number of digits after dot. (optional) 
+ordering = yes # Is ordering defined for timestamps? # (optional, default: no) 
+tsa_name = yes # Must the TSA name be included in the reply? # (optional, default: no) 
+ess_cert_id_chain = no # Must the ESS cert id chain be included? # (optional, default: no) 
+## end of openssl.cnf snippet ##
+```
+
---
+++ server.groovy
@@ -0,0 +1,160 @@
+#!/usr/bin/env groovy
+
+//parameters
+def PORT = 318;
+def OPENSSL_CFG_FILE = "/etc/ssl/openssl.cnf";
+def openssl = "openssl"; ///usr/bin/openssl
+
+/*
+Copyright [2015] [Jan Zajic]
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import com.sun.net.httpserver.*;
+import java.util.concurrent.Executors;
+import java.io.File
+import java.io.InputStream
+import java.io.OutputStream
+import java.io.FileOutputStream
+import java.nio.file.Files;
+
+println "Starting server on port $PORT"
+
+//configuring a Java 6 HttpServer
+InetSocketAddress addr = new InetSocketAddress(PORT)
+httpServer = com.sun.net.httpserver.HttpServer.create(addr, 0)
+httpServer.with {
+    createContext('/', new ReverseHandler(OPENSSL_CFG_FILE,openssl))
+    setExecutor(Executors.newCachedThreadPool())
+    start()
+}
+
+//test it:
+// curl -X POST -H "Content-Type:application/timestamp-query" -d @myfilename localhost:318 -v
+
+class ReverseHandler implements HttpHandler {
+	
+	private String openSslCfg;
+	private String openSslBin;
+	
+	ReverseHandler(String openSslCfg, String openSslBin)
+	{
+		this.openSslBin = openSslBin;
+		this.openSslCfg= openSslCfg;
+	}
+	
+	private void handleTimestampRequest(HttpExchange httpExchange)
+	{	
+		File input = null;
+		File output = null;
+		byte[] buf = new byte[2048];
+		int read = 0;
+		
+		try {
+			println("handleTimestampRequest");
+			input = File.createTempFile("req-in", ".tmp");
+			output = File.createTempFile("req-out", ".tmp");
+			input.deleteOnExit();						
+			output.deleteOnExit();						
+		} catch(RuntimeException e) {
+			e.printStackTrace();
+			return;
+		}
+		
+		try {	
+			println(input.absolutePath);
+			
+			InputStream body;
+			OutputStream inputFileOutStream;
+			//copy request to temp file
+			try
+			{
+				body = httpExchange.getRequestBody();
+				inputFileOutStream = new FileOutputStream(input);
+				
+				while ((read = body.read(buf)) != -1) {
+					inputFileOutStream.write(buf, 0, read);
+				}
+				println("request written to file "+input.absolutePath)
+			} finally {
+				body.close(); inputFileOutStream.close();
+			}
+			
+			def sout = new StringBuffer(), serr = new StringBuffer()
+			println("starting "+openSslBin);
+			def proc = "$openSslBin ts -reply -config $openSslCfg -queryfile $input.absolutePath -out $output.absolutePath".execute()			
+			println("$openSslBin wait")
+			proc.waitForProcessOutput(sout, serr)			
+			println("$openSslBin end")
+			def exitValue = proc.exitValue()
+			//log
+			println("openssl result "+exitValue);
+			println(serr.toString())
+			println(sout.toString())
+			println("response written to file "+output.absolutePath)
+			
+			httpExchange.responseHeaders.set('Content-Type', 'application/timestamp-reply')
+			httpExchange.sendResponseHeaders(200, 0)
+			
+			//write response to client
+			InputStream outputFileInStream;
+			OutputStream out;
+			
+			try
+			{
+				outputFileInStream = new FileInputStream(output);
+				out = httpExchange.getResponseBody();
+				
+				while ((read = outputFileInStream.read(buf)) != -1) {
+					out.write(buf, 0, read);
+				}
+				out.flush();
+			} finally {
+				outputFileInStream.close(); out.close();
+			}			
+			
+			httpExchange.close();
+		} catch(Exception e) {
+			e.printStackTrace();
+			httpExchange.sendResponseHeaders(400,0)
+			httpExchange.responseBody.close()
+		} finally {
+			input.delete();
+			output.delete();
+		}
+	}
+	
+    @Override
+    public void handle(HttpExchange httpExchange) throws IOException
+    {
+		try {
+			String requestMethod = httpExchange.requestMethod
+			println "requestMethod: '"+requestMethod+"'"
+			if(requestMethod == 'POST') {				
+				String type = httpExchange.requestHeaders.get('Content-Type')
+				println "Content-Type: "+type
+				if(type == "[application/timestamp-query]")
+				{
+					println("timestamp-query");
+					handleTimestampRequest(httpExchange)
+					return;
+				}				
+			}
+			httpExchange.sendResponseHeaders(400,0)
+			httpExchange.responseBody.close()
+		} catch(RuntimeException e) {
+			e.printStackTrace();
+		}		
+    }
+}
